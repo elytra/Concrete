b3ae0093b4b337879991bb4e078ab033662e3b0e
Jamie Mansfield <dev@jamierocks.uk>
2017-03-16 21:14:14 +0000

    Drop _PRESET

--- src/main/java/com/elytradev/concrete/block/Presets.java
+++ src/main/java/com/elytradev/concrete/block/Presets.java
@@ -15,14 +15,14 @@ public final class Presets {
      *
      * @see ConcreteBlock.Builder#preset(Consumer)
      */
-    public static Consumer<ConcreteBlock.Builder> ORE_PRESET = builder -> builder.drop(ItemDropBehaviour.Fortune.DEFAULT);
+    public static Consumer<ConcreteBlock.Builder> ORE= builder -> builder.drop(ItemDropBehaviour.Fortune.DEFAULT);
 
     /**
      * A preset for a block builder with glass properties, such as the hardness, and resistance.
      *
      * @see ConcreteBlock.Builder#preset(Consumer)
      */
-    public static Consumer<ConcreteBlock.Builder> GLASS_PRESET = builder -> builder.material(Material.GLASS)
+    public static Consumer<ConcreteBlock.Builder> GLASS = builder -> builder.material(Material.GLASS)
             .soundType(SoundType.GLASS)
             .drop(ItemDropBehaviour.DROP_NONE)
             .hardness(0.3f)
