d8fc8041be7b17fdd420978779fd7e2b175a50de
Aesen Vismea <aesen@unascribed.com>
2017-04-26 15:11:59 -0400

    Fix license headers

--- src/main/java/com/elytradev/concrete/inventory/gui/client/GuiDrawing.java
+++ src/main/java/com/elytradev/concrete/inventory/gui/client/GuiDrawing.java
@@ -25,6 +25,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
 package com.elytradev.concrete.inventory.gui.client;
 
 import org.lwjgl.opengl.GL11;
