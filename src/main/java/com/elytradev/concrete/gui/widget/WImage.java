190b452a24d7053d98e1b02a21ba0bfbbba7fc29
Falkreon <falkreon@gmail.com>
2017-04-26 13:53:00 -0500

    Add gui classes (#6)

---
+++ src/main/java/com/elytradev/concrete/gui/widget/WImage.java
@@ -0,0 +1,50 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2017 Isaac Ellingson (Falkreon) and contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.elytradev.concrete.gui.widget;
+
+import com.elytradev.concrete.client.gui.GuiDrawing;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class WImage extends WWidget {
+	ResourceLocation texture;
+	
+	public WImage(ResourceLocation loc) {
+		this.texture = loc;
+	}
+	
+	
+	@Override
+	public boolean canResize() {
+		return true;
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void paintBackground(int x, int y) {
+		GuiDrawing.rect(texture, x, y, getWidth(), getHeight(), 0xFFFFFFFF);
+	}
+}
