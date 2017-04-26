eca1f82884ad5519dfe2baca70a0d4963f08c036
Aesen Vismea <aesen@unascribed.com>
2017-04-26 15:10:46 -0400

    License headers, better README, add Inventory module

--- src/main/java/com/elytradev/concrete/inventory/gui/client/ConcreteGui.java
+++ src/main/java/com/elytradev/concrete/inventory/gui/client/ConcreteGui.java
@@ -1,14 +1,18 @@
-/**
- * MIT License
+/*
+ * The MIT License (MIT)
  *
- * Copyright (c) 2017 Isaac Ellingson (Falkreon) and contributors
+ * Copyright (c) 2016-2017:
+ * 	Una Thompson (unascribed),
+ * 	Isaac Ellingson (Falkreon),
+ * 	Jamie Mansfield (jamierocks),
+ * 	and contributors
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
@@ -21,11 +25,11 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package com.elytradev.concrete.client.gui;
+package com.elytradev.concrete.inventory.gui.client;
 
 import java.io.IOException;
 
-import com.elytradev.concrete.gui.ConcreteContainer;
+import com.elytradev.concrete.inventory.gui.ConcreteContainer;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
