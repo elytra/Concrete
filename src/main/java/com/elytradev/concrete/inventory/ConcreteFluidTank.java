6dfb9701d82df6b526c6b2148a6bae25cd378277
Ethan Brooks <EB5473@gmail.com>
2017-04-28 02:50:52 -0400

    Copied and modified javadocs from ConcreteItemStorage for FluidTank

--- src/main/java/com/elytradev/concrete/inventory/ConcreteFluidTank.java
+++ src/main/java/com/elytradev/concrete/inventory/ConcreteFluidTank.java
@@ -28,15 +28,55 @@
 
 package com.elytradev.concrete.inventory;
 
+import net.minecraft.util.EnumFacing;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTank;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.function.Predicate;
 
+
+/**
+ * A base class for managing fluid storage.
+ *
+ * <h2>Validation</h2>
+ *
+ * Just like {@link ConcreteItemStorage}, ConcreteFluidTank <em>does not</em> perform its own validation. Rather, this
+ * is offloaded to its own wrapper for such functionality, {@link ValidatedFluidTankWrapper}.
+ *
+ * <h2>Serialization and Deserialization</h2>
+ *
+ * <p>If you're using this object to manage the fluid tank of a TileEntity, it takes three small tweaks to get no-fuss
+ * serialization. In your constructor, add
+ *
+ * <code><pre>fluidTank.listen(this::markDirty);</pre></code>
+ *
+ * <p>This will mark your tile dirty any time the inventory changes, so that Minecraft won't skip serialization. Then in
+ * writeToNBT:
+ *
+ * <code><pre>tagOut.setTag("fluid_tank", CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY.writeNBT(fluidTank, null));</pre></code>
+ *
+ * <p>and in readFromNBT:
+ *
+ * <code><pre>CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY.readNBT(fluidTank, null, tag.getTag("fluid_tank"));</pre></code>
+ *
+ * <p>Where <code>fluidTank</code> is your ConcreteFluidTank object. At that point the Forge Capability system will
+ * do all the work for you on this, and you can focus on the interesting part of making your tile do what it's supposed to.
+ *
+ * <h2>Exposing as a capability</h2>
+ *
+ * <p>This was mentioned above in the Validation section, but although this object supplies the IFluidHandler interface,
+ * generally you want to create a ValidatedFluidTankWrapper instead. These wrappers are okay to cache or memoize,
+ * and merely provide a succinct delegation based on the access rules this object provides.
+ *
+ * <p>Simplifying hasCapability and getCapability are outside the scope of this object... but remember that "null" is a
+ * valid side, and often represents the side a probe observer accesses, so plan your views accordingly.
+ *
+ */
 public class ConcreteFluidTank extends FluidTank implements IObservableFluidTank {
     private ArrayList<Runnable> listeners = new ArrayList<>();
     private Predicate<FluidStack> fillValidator = Validators.ANY_FLUID;
