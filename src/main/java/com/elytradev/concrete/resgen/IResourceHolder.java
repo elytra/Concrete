fca9f3fd218fc27b97f0ee898b9c15168adeb73d
darkevilmac <bk1325@gmail.com>
2017-05-02 23:55:31 -0700

    Add ConcreteResourcePack.

---
+++ src/main/java/com/elytradev/concrete/resgen/IResourceHolder.java
@@ -0,0 +1,12 @@
+package com.elytradev.concrete.resgen;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public interface IResourceHolder {
+
+	@SideOnly(Side.CLIENT)
+	ResourceLocation getResource(EnumResourceType resourceType, int meta);
+
+}
---
+++ src/main/java/com/elytradev/concrete/resgen/MissingRealpackException.java
@@ -0,0 +1,10 @@
+package com.elytradev.concrete.resgen;
+
+/**
+ * Thrown when a ConcreteResourcePack can't find a valid fallback pack to use.
+ */
+public class MissingRealpackException extends RuntimeException {
+	public MissingRealpackException(String modID) {
+		super("Missing valid fallback pack for " + modID);
+	}
+}
