a00ae531b3e226d6ab1fd0f70e678745c4cb331a
Ethan Brooks <EB5473@gmail.com>
2017-05-26 23:07:21 -0400

    Add WFluidBar, WPlainPanel, bits of fluid handling (#10)
    * Work in progress - basic fluid tank logic for inspection
    
    * No more drain validators, added validator wrapper
    
    * Copied and modified javadocs from ConcreteItemStorage for FluidTank
    
    * First attempt at faking a slot for fluid sync
    
    * Possibly better fake fluid slot
    
    * FluidSlot improvements, FluidBar UP rendering implemented.
    
    * Fix things.
    
    * Added a Fluid rendering method for fun
    
    * Fix FluidBar
    
    * Actual UV scaling
    
    * Remove out spam
    
    * Working Fluid Rendering
    
    * First draft upwards fluids
    
    * License problems
    
    * More Concrete work
    
    * Added last bit of work to WFluidBar
    
    * L I C E N S E S   A R E   H A R D
    
    * Whoops, optimize dependencies
    
    * Uncomplicate arguments

---
+++ src/main/java/com/elytradev/concrete/inventory/ConcreteFluidTank.java
@@ -0,0 +1,123 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2017:
+ * 	William Thompson (unascribed),
+ * 	Isaac Ellingson (Falkreon),
+ * 	Jamie Mansfield (jamierocks),
+ * 	and contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.elytradev.concrete.inventory;
+
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidTank;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+
+/**
+ * A base class for managing fluid storage.
+ *
+ * <h2>Validation</h2>
+ *
+ * Just like {@link ConcreteItemStorage}, ConcreteFluidTank <em>does not</em> perform its own validation. Rather, this
+ * is offloaded to its own wrapper for such functionality, {@link ValidatedFluidTankWrapper}.
+ *
+ * <h2>Serialization and Deserialization</h2>
+ *
+ * <p>If you're using this object to manage the fluid tank of a TileEntity, it takes three small tweaks to get no-fuss
+ * serialization. In your constructor, add
+ *
+ * <code><pre>fluidTank.listen(this::markDirty);</pre></code>
+ *
+ * <p>This will mark your tile dirty any time the inventory changes, so that Minecraft won't skip serialization. Then in
+ * writeToNBT:
+ *
+ * <code><pre>tagOut.setTag("fluid_tank", CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY.writeNBT(fluidTank, null));</pre></code>
+ *
+ * <p>and in readFromNBT:
+ *
+ * <code><pre>CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY.readNBT(fluidTank, null, tag.getTag("fluid_tank"));</pre></code>
+ *
+ * <p>Where <code>fluidTank</code> is your ConcreteFluidTank object. At that point the Forge Capability system will
+ * do all the work for you on this, and you can focus on the interesting part of making your tile do what it's supposed to.
+ *
+ * <h2>Exposing as a capability</h2>
+ *
+ * <p>This was mentioned above in the Validation section, but although this object supplies the IFluidHandler interface,
+ * generally you want to create a ValidatedFluidTankWrapper instead. These wrappers are okay to cache or memoize,
+ * and merely provide a succinct delegation based on the access rules this object provides.
+ *
+ * <p>Simplifying hasCapability and getCapability are outside the scope of this object... but remember that "null" is a
+ * valid side, and often represents the side a probe observer accesses, so plan your views accordingly.
+ *
+ */
+public class ConcreteFluidTank extends FluidTank implements IObservableFluidTank {
+    private ArrayList<Runnable> listeners = new ArrayList<>();
+    private Predicate<FluidStack> fillValidator = Validators.ANY_FLUID;
+
+    public ConcreteFluidTank(int capacity) {
+        this(null, capacity);
+    }
+
+    public ConcreteFluidTank(Fluid fluid, int amount, int capacity) {
+        this(new FluidStack(fluid, amount), capacity);
+    }
+
+    public ConcreteFluidTank(@Nullable FluidStack fluidStack, int capacity) {
+        super(fluidStack, capacity);
+    }
+
+    public final ConcreteFluidTank withFillValidator(Predicate<FluidStack> fillValidator) {
+        this.fillValidator = fillValidator;
+        return this;
+    }
+
+
+    public void markDirty() {
+        for(Runnable r : listeners) {
+            r.run();
+        }
+    }
+
+    @Override
+    public void listen(@Nonnull Runnable r) {
+        listeners.add(r);
+    }
+
+    @Override
+    protected void onContentsChanged() {
+        this.markDirty();
+        super.onContentsChanged();
+    }
+
+    @Nonnull
+    public Predicate<FluidStack> getFillValidator() {
+        return this.fillValidator;
+    }
+
+
+}
---
+++ src/main/java/com/elytradev/concrete/inventory/FluidTankProxySlot.java
@@ -0,0 +1,124 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2017:
+ * 	William Thompson (unascribed),
+ * 	Isaac Ellingson (Falkreon),
+ * 	Jamie Mansfield (jamierocks),
+ * 	and contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.elytradev.concrete.inventory;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Manages syncing of {@code FluidStack} data between server/client, utilizing "fake" items encapsulating {@code FluidStack} data.
+ *
+ * <h2>Overview</h2>
+ *
+ * <p>FluidTankProxySlot is setup like any other {@code Slot}, with the exception that the {@code IInventory} it's supposed to reference
+ * doesn't actually exist - instead, all {@code IInventory} referencing functions
+ * (e.g. {@link #getStack()}, {@link #putStack(ItemStack)}, {@link #onSlotChanged()}, etc.) are all overridden to
+ * serialize/deserialize information about the participating {@link ConcreteFluidTank} (namely, its {@code FluidStack}).</p>
+ *
+ * <p>This allows for super easy syncing of the FluidStack data between the server and the client, allowing
+ * {@link com.elytradev.concrete.inventory.gui.widget.WWidget}s like
+ * {@link com.elytradev.concrete.inventory.gui.widget.WFluidBar} to work and sync properly.</p>
+ *
+ */
+public class FluidTankProxySlot extends Slot {
+    private ConcreteFluidTank delegate;
+
+    public FluidTankProxySlot(ConcreteFluidTank delegate) {
+        super(null, 0, Integer.MIN_VALUE,Integer.MIN_VALUE);
+        this.delegate = delegate;
+    }
+
+    @Override
+    @Nonnull
+    public ItemStack getStack() {
+        NBTTagCompound fluidTank = new NBTTagCompound();
+        NBTTagCompound fluidStack = new NBTTagCompound();
+
+        /* Having a little fun with it... */
+        NBTTagCompound display = new NBTTagCompound();
+        NBTTagList garbageLore = new NBTTagList();
+        garbageLore.appendTag(new NBTTagString("What? How can you see this?"));
+        garbageLore.appendTag(new NBTTagString("Tell @CalmBit immediately."));
+        display.setTag("Name", new NBTTagString("Fluid Stick"));
+        display.setTag("Lore", garbageLore);
+        /* End fun */
+
+        delegate.writeToNBT(fluidStack);
+        fluidTank.setTag("fluid_tank", fluidStack);
+        fluidTank.setTag("display", display);
+        ItemStack result = new ItemStack(Items.STICK, 1, 0);
+        result.setTagCompound(fluidTank);
+        return result;
+    }
+
+    @Override
+    public void putStack(@Nonnull ItemStack stack) {
+        if (stack.getTagCompound() != null && stack.getTagCompound().hasKey("fluid_tank"))
+            delegate.readFromNBT(stack.getTagCompound().getCompoundTag("fluid_tank"));
+    }
+
+    @Override
+    public void onSlotChanged() {
+        this.delegate.markDirty();
+    }
+
+    @Override
+    public int getSlotStackLimit() {
+        return 1;
+    }
+
+    @Override
+    @Nonnull
+    public ItemStack decrStackSize(int amount) {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public boolean isHere(IInventory inv, int slotIn) {
+        return false;
+    }
+
+    @Override
+    public boolean isSameInventory(Slot other) {
+        if (other instanceof FluidTankProxySlot) {
+            FluidTankProxySlot slot = (FluidTankProxySlot)other;
+            if( slot.delegate == this.delegate)
+                return true;
+        }
+        return false;
+    }
+}
