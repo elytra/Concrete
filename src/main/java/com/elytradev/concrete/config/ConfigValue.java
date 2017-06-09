fceeb3a26b7fa5c607695e8778001e3e1f3aaa31
Benjamin K <bk1325@gmail.com>
2017-06-09 16:01:28 -0700

    Add Config Module (#12)
    * Add Concrete Config module code.
    
    * Replace spaces with tabs.
    
    * Add Config module to README.md
    
    * Add Config module to build

---
+++ src/main/java/com/elytradev/concrete/config/ConfigValue.java
@@ -0,0 +1,38 @@
+package com.elytradev.concrete.config;
+
+import net.minecraftforge.common.config.Property;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Used on fields in a ConcreteConfig implementation.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface ConfigValue {
+
+	/**
+	 * The configuration comment for this field, provides information to end users.
+	 * There's very few cases where you shouldn't include this.
+	 */
+	String comment() default "";
+
+	/**
+	 * The configuration category for this field. Leave empty if not applicable.
+	 */
+	String category() default "";
+
+	/**
+	 * The field key, the 'name' used in the configuration file. Leave empty if the field name should be used.
+	 */
+	String key() default "";
+
+	/**
+	 * The property type for this config field. Determines how the field will be serialized.
+	 */
+	Property.Type type();
+
+}
