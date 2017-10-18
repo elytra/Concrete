9ff3673a93772bec6e77bc372d15e92e43160f2f
Ethan Brooks <EB5473@gmail.com>
2017-10-18 16:22:53 -0400

    Actually fleshed out implementation of a toast

--- src/main/java/com/elytradev/concrete/common/ExtensiveToast.java
+++ src/main/java/com/elytradev/concrete/common/ExtensiveToast.java
@@ -7,63 +7,70 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 @SideOnly(Side.CLIENT)
 public class ExtensiveToast implements IToast {
 
-    private ResourceLocation texture;
-    private String title;
-    private String subtitle;
-    private int titleColor;
-    private int subtitleColor;
-    private int textureX, textureY;
+	private ResourceLocation texture;
+	private String title;
+	private String subtitle;
+	private long timing;
+	private int titleColor;
+	private int subtitleColor;
+	private int textureX, textureY;
 
-    private static int DEFAULT_X = 0;
-    private static int DEFAULT_Y = 96;
+	public static final long DEFAULT_TIMING = 5000;
+	public static final int DEFAULT_X = 0;
+	public static final int DEFAULT_Y = 96;
 
-    public ExtensiveToast(String title) {
-        this(title, null, TEXTURE_TOASTS.toString(), -256, -1, DEFAULT_X, DEFAULT_Y);
-    }
+	public ExtensiveToast(@Nonnull String title) {
+		this(title, null, DEFAULT_TIMING, TEXTURE_TOASTS.toString(), -256, -1, DEFAULT_X, DEFAULT_Y);
+	}
 
-    public ExtensiveToast(String title, String subtitle) {
-        this(title, subtitle, TEXTURE_TOASTS.toString(), -256, -1, DEFAULT_X, DEFAULT_Y);
-    }
+	public ExtensiveToast(@Nonnull String title, @Nullable String subtitle) {
+		this(title, subtitle, DEFAULT_TIMING, TEXTURE_TOASTS.toString(), -256, -1, DEFAULT_X, DEFAULT_Y);
+	}
 
-    public ExtensiveToast(String title, String subtitle, String texture, int titleColor, int subtitleColor) {
-        this(title, subtitle, texture, titleColor, subtitleColor, 0, 0);
-    }
+	public ExtensiveToast(@Nonnull String title, @Nullable String subtitle, long timing) {
+		this(title, subtitle, timing, TEXTURE_TOASTS.toString(), -256, -1, DEFAULT_X, DEFAULT_Y);
+	}
 
-    public ExtensiveToast(String title,
-                          String subtitle,
-                          String texture,
-                          int titleColor,
-                          int subtitleColor,
-                          int textureX,
-                          int textureY) {
-        this.title = title;
-        this.subtitle = subtitle;
-        this.texture = new ResourceLocation(texture);
-        this.titleColor = titleColor;
-        this.subtitleColor = subtitleColor;
-        this.textureX = textureX;
-        this.textureY = textureY;
-    }
+	public ExtensiveToast(@Nonnull String title,
+						  @Nullable String subtitle,
+						  long timing,
+						  @Nonnull String texture,
+						  int titleColor,
+						  int subtitleColor,
+						  int textureX,
+						  int textureY) {
+		this.title = title;
+		this.subtitle = subtitle;
+		this.timing = timing;
+		this.texture = new ResourceLocation(texture);
+		this.titleColor = titleColor;
+		this.subtitleColor = subtitleColor;
+		this.textureX = textureX;
+		this.textureY = textureY;
+	}
 
-    @Override
-    public Visibility draw(GuiToast toastGui, long delta) {
-        toastGui.getMinecraft().getTextureManager().bindTexture(TEXTURE_TOASTS);
-        GlStateManager.color(1.0F, 1.0F, 1.0F);
-        toastGui.drawTexturedModalRect(0, 0, textureX, textureY, 160, 32);
+	@Override
+	public Visibility draw(GuiToast toastGui, long delta) {
+		toastGui.getMinecraft().getTextureManager().bindTexture(TEXTURE_TOASTS);
+		GlStateManager.color(1.0F, 1.0F, 1.0F);
+		toastGui.drawTexturedModalRect(0, 0, textureX, textureY, 160, 32);
 
-        if (this.subtitle == null)
-        {
-            toastGui.getMinecraft().fontRenderer.drawString(this.title, 18, 12, titleColor);
-        }
-        else {
-            toastGui.getMinecraft().fontRenderer.drawString(this.title, 18, 7, titleColor);
-            toastGui.getMinecraft().fontRenderer.drawString(this.subtitle, 18, 18, subtitleColor);
-        }
+		if (this.subtitle == null)
+		{
+			toastGui.getMinecraft().fontRenderer.drawString(this.title, 18, 12, titleColor);
+		}
+		else {
+			toastGui.getMinecraft().fontRenderer.drawString(this.title, 18, 7, titleColor);
+			toastGui.getMinecraft().fontRenderer.drawString(this.subtitle, 18, 18, subtitleColor);
+		}
 
-        return IToast.Visibility.SHOW;
-    }
+		return delta < timing ? Visibility.SHOW : Visibility.HIDE;
+	}
 
 }
--- src/main/java/com/elytradev/concrete/utilpackets/DisplayToastMessage.java
+++ src/main/java/com/elytradev/concrete/utilpackets/DisplayToastMessage.java
@@ -6,36 +6,88 @@ import com.elytradev.concrete.network.NetworkContext;
 import com.elytradev.concrete.network.annotation.field.MarshalledAs;
 import com.elytradev.concrete.network.annotation.type.ReceivedOn;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.toasts.SystemToast;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import static net.minecraft.client.gui.toasts.IToast.TEXTURE_TOASTS;
 
 @ReceivedOn(Side.CLIENT)
 public class DisplayToastMessage extends Message {
 
-    @MarshalledAs("string")
-    public String title;
+	@MarshalledAs("string")
+	public String title;
 
-    @MarshalledAs("string")
-    public String subtitle;
+	@MarshalledAs("string")
+	public String subtitle;
 
-    public DisplayToastMessage(NetworkContext ctx) {
-        super(ctx);
-    }
+	@MarshalledAs("i64")
+	public long timing;
 
-    public DisplayToastMessage(NetworkContext ctx, String title, String subtitle) {
-        super(ctx);
-        this.title = title;
-        this.subtitle = subtitle;
-    }
+	@MarshalledAs("string")
+	public String texture;
 
-    @Override
-    @SideOnly(Side.CLIENT)
-    protected void handle(EntityPlayer player) {
-        //Minecraft.getMinecraft().getToastGui().add(new ExtensiveToast(title, subtitle));
-        Minecraft.getMinecraft().getToastGui().add(new SystemToast(SystemToast.Type.NARRATOR_TOGGLE,
-                new TextComponentString("hello"),new TextComponentString("World")));
-    }
+	@MarshalledAs("i32")
+	public int titleColor;
+
+    @MarshalledAs("i32")
+	public int subtitleColor;
+
+    @MarshalledAs("i32")
+	public int textureX;
+
+    @MarshalledAs("i32")
+	public int textureY;
+
+	public static final long DEFAULT_TIMING = 5000;
+	private static final int DEFAULT_TITLE_COLOR = -256;
+	private static final int DEFAULT_SUBTITLE_COLOR = -1;
+	public static final int DEFAULT_X = 0;
+	public static final int DEFAULT_Y = 96;
+
+	public DisplayToastMessage(NetworkContext ctx) {
+		super(ctx);
+	}
+
+	public DisplayToastMessage(NetworkContext ctx, @Nonnull String title) {
+		this(ctx, title, null, DEFAULT_TIMING, TEXTURE_TOASTS.toString(), DEFAULT_TITLE_COLOR, DEFAULT_SUBTITLE_COLOR, DEFAULT_X, DEFAULT_Y);
+	}
+
+	public DisplayToastMessage(NetworkContext ctx, @Nonnull String title, @Nullable String subtitle) {
+		this(ctx, title, subtitle, DEFAULT_TIMING, TEXTURE_TOASTS.toString(), DEFAULT_TITLE_COLOR, DEFAULT_SUBTITLE_COLOR, DEFAULT_X, DEFAULT_Y);
+	}
+
+	public DisplayToastMessage(NetworkContext ctx, @Nonnull String title, @Nullable String subtitle, long timing) {
+		this(ctx, title, subtitle, timing, TEXTURE_TOASTS.toString(), DEFAULT_TITLE_COLOR, DEFAULT_SUBTITLE_COLOR, DEFAULT_X, DEFAULT_Y);
+	}
+
+
+	public DisplayToastMessage(NetworkContext ctx,
+                               @Nonnull String title,
+                               @Nullable String subtitle,
+                               long timing,
+                               @Nonnull String texture,
+                               int titleColor,
+                               int subtitleColor,
+                               int textureX,
+                               int textureY) {
+	    super(ctx);
+	    this.title = title;
+	    this.subtitle = subtitle;
+	    this.timing = timing;
+	    this.texture = texture;
+        this.titleColor  = titleColor;
+        this.subtitleColor = subtitleColor;
+        this.textureX = textureX;
+        this.textureY = textureY;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	protected void handle(EntityPlayer player) {
+		Minecraft.getMinecraft().getToastGui().add(new ExtensiveToast(title, subtitle, timing, texture, titleColor, subtitleColor, textureX, textureY));
+	}
 }
