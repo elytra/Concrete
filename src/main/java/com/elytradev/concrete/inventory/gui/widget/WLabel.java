6606d8c0dbaa25d797b56e624f62f7c05fde6527
Falkreon <falkreon@gmail.com>
2018-06-11 16:15:50 -0400

    BREAKING: Replace WFieldedLabel and WLabel with a field-capable WLabel that doesn't break SMP

--- src/main/java/com/elytradev/concrete/inventory/gui/widget/WLabel.java
+++ src/main/java/com/elytradev/concrete/inventory/gui/widget/WLabel.java
@@ -30,27 +30,53 @@
 package com.elytradev.concrete.inventory.gui.widget;
 
 import com.elytradev.concrete.inventory.gui.client.GuiDrawing;
-import net.minecraft.client.Minecraft;
+import com.elytradev.concrete.inventory.gui.widget.WWidget;
+
+import net.minecraft.inventory.IInventory;
 
 public class WLabel extends WWidget {
-	protected final String text;
-	protected final int color;
-
 	public static final int DEFAULT_TEXT_COLOR = 0x404040;
-
+	protected String text;
+	protected int color;
+	
+	protected IInventory inventory;
+	protected int field1 = -1;
+	protected int field2 = -1;
+	
 	public WLabel(String text, int color) {
 		this.text = text;
 		this.color = color;
-		this.setSize(Minecraft.getMinecraft().fontRenderer.getStringWidth(text), 8);
 	}
-
+	
 	public WLabel(String text) {
 		this(text, DEFAULT_TEXT_COLOR);
 	}
-
+	
+	public WLabel withFields(IInventory inv, int field1, int field2) {
+		this.inventory = inv;
+		this.field1 = field1;
+		this.field2 = field2;
+		return this;
+	}
+	
 	@Override
 	public void paintBackground(int x, int y) {
-		GuiDrawing.drawString(text, x, y, color);
+		int field1Contents = 0;
+		int field2Contents = 0;
+		if (inventory!=null) {
+			if (field1>=0) {
+				field1Contents = inventory.getField(field1);
+			}
+			
+			if (field2>=0) {
+				field2Contents = inventory.getField(field2);
+			}
+		}
+		
+		@SuppressWarnings("deprecation")
+		String formatted = net.minecraft.util.text.translation.I18n.translateToLocalFormatted(text, field1Contents, field2Contents);
+		
+		GuiDrawing.drawString(formatted, x, y, color);
 	}
 
 	@Override
